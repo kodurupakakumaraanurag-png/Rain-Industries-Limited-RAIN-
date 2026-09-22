3f3751e62def8cf72037e75ebc8410d0ea0f2816
kodurupakakumaraanurag-png <kodurupakakumaraanurag@gmail.com>
2026-09-22 20:43:48 +0530

    fix(vercel): include sqlite db, add DATABASE_URL fallback and outputFileTracing for Vercel deployment

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingIncludes: {
+    '/api/**/*': ['./prisma/**/*'],
+    '/**/*': ['./prisma/**/*'],
+  },
 };
 
 module.exports = nextConfig;
